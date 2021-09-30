14eb0536e446ee7d158d3e541dca64cd4e9ff452
Angelica <angelicapedroso05@gmail.com>
2021-09-30 15:40:41 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (name === undefined || age === undefined) {
+    return undefined;
+  }
+  const phrase = `Oi, meu nome é ${name}!\n` +
+    `Tenho ${age} anos,\n` +
+    'trabalho na Trybe e mando muito em programação!\n' +
+    '#VQV!';
+    
+    return phrase;
+};
 
 module.exports = vqv;
